f66600b85035600cc47b678d749eb579e48a726c
Vadim Semchenka <vadimsemch@gmail.com>
2021-08-22 12:54:14 +0300

    cor html

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,32 +1,32 @@
 const accordeon = () => {
-    const accordeon = document.querySelector('.accordeon'),
-          element = accordeon.querySelectorAll('.element');
-  
-    const addStyle = () => {
-      let style = document.createElement('style');
-      style.textContent = `
-        .accordeon .element.active .element-content {
-          display: block !important;
-        }
-        .accordeon .element-content {
-          display: none !important;
-        }
-      `;
-      document.head.appendChild(style);
-    };
-    addStyle();
-  
-    for(let item of element) {
-      item.addEventListener('click', function() {
-        if(this.classList.contains('active')){
-          this.classList.remove('active');
-        } else {
-          for(let elem of element){
-            elem.classList.remove('active');
-          }
-          this.classList.add('active');
-        }
-      });
-    }
+  const accordeon = document.querySelector('.accordeon'),
+        element = accordeon.querySelectorAll('.element');
+
+  const addStyle = () => {
+    let style = document.createElement('style');
+    style.textContent = `
+      .accordeon .element.active .element-content {
+        display: block !important;
+      }
+      .accordeon .element-content {
+        display: none !important;
+      }
+    `;
+    document.head.appendChild(style);
   };
-  export default accordeon;
+  addStyle();
+
+  for(let item of element) {
+    item.addEventListener('click', function() {
+      if(this.classList.contains('active')){
+        this.classList.remove('active');
+      } else {
+        for(let elem of element){
+          elem.classList.remove('active');
+        }
+        this.classList.add('active');
+      }
+    });
+  }
+};
+export default accordeon;
