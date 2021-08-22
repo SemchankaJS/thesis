3ed4a410c4b9cb19469b74772341a4ca192c7b2b
Vadim Semchenka <vadimsemch@gmail.com>
2021-08-22 12:29:17 +0300

    correct scroll

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,20 +1,32 @@
-
 const accordeon = () => {
-    const elements = document.querySelectorAll('.accordeon .element');
-    const element = document.querySelector('.accordeon');
+    const accordeon = document.querySelector('.accordeon'),
+          element = accordeon.querySelectorAll('.element');
   
-    element.addEventListener('click', event => {
-        event.preventDefault();
-        const target = event.target.closest('.element');
-  
-        if (target.classList.contains('active')) {
-            target.classList.remove('active');
-        } else {
-            elements.forEach(elem => {
-                elem.classList.remove('active');
-            });
-            target.classList.add('active');
+    const addStyle = () => {
+      let style = document.createElement('style');
+      style.textContent = `
+        .accordeon .element.active .element-content {
+          display: block !important;
         }
-    });
-};
-export default accordeon;
+        .accordeon .element-content {
+          display: none !important;
+        }
+      `;
+      document.head.appendChild(style);
+    };
+    addStyle();
+  
+    for(let item of element) {
+      item.addEventListener('click', function() {
+        if(this.classList.contains('active')){
+          this.classList.remove('active');
+        } else {
+          for(let elem of element){
+            elem.classList.remove('active');
+          }
+          this.classList.add('active');
+        }
+      });
+    }
+  };
+  export default accordeon;
